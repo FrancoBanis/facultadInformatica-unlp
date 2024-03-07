9aec2d9701cdc0c3db1321f658b5720edc7098cf
Franco Banis <franco.banis0@gmail.com>
2024-03-07 20:39:14 -0300

    Se agrego el repositorio segundo y el repositorio FOD dentro de este , insertando el primer y segundo ejercicio de la practica

---
+++ segundo/FOD/ejercicio01.pas
@@ -0,0 +1,40 @@
+program ejercicio01;
+const
+    CORTE = 30000;
+    CONDICION = 1500;
+type
+    archNum = file of integer;
+var
+    nomEl : string;
+    archivoNumeros : archNum;
+    numLeido : integer;
+    i,numAct : integer;
+    total : integer;
+    contador : integer;
+    prom : real;
+begin
+  write('Ingresar nombre del archivo: ');
+  readln(nomEl);
+  Assign(archivoNumeros, nomEl);
+  Rewrite(archivoNumeros);
+  write('Ingresar numero: ');
+  readln(numLeido);
+  while( numLeido <> CORTE) do begin
+    write(archivoNumeros,numLeido);
+    write('Ingresar numero: ');
+    readln(numLeido);
+  end;  
+  total := filepos(archivoNumeros);
+  writeln('Total: ', total);
+  for i:=filepos(archivoNumeros)-1 downto 0  do begin
+    Seek(archivoNumeros,filepos(archivoNumeros)-1);
+    read(archivoNumeros,numAct);
+    if (numAct < CONDICION) then 
+        writeln(numAct);
+    contador := contador + numAct;
+    Seek(archivoNumeros,filepos(archivoNumeros)-1);
+    end;
+    prom := contador / total;
+    writeln('Promedio   : ', prom);
+    Close(archivoNumeros);
+end.
